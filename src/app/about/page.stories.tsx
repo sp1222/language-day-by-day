06762658726362612849fc7cc50b12dc33eb6811
sp1222 <s.polozoff@gmail.com>
2024-07-22 21:10:06 -0500

    working implementation of storybook

---
+++ src/app/about/page.stories.tsx
@@ -0,0 +1,13 @@
+import About from "./page";
+
+export default {
+  component: About,
+  title: "About",
+  tags: ["autodocs"],
+  excludeStories: /.*Data$/,
+};
+
+const Template = (args: any) => <About {...args} />;
+
+export const Default = Template.bind({});
+const args = {};
