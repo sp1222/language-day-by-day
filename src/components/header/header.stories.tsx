53a9f2a739347dadd681c3f1ca8cc57fb0e3f393
sp1222 <s.polozoff@gmail.com>
2024-07-24 20:42:37 -0500

    happy path story for Header

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -5,6 +5,9 @@ import { Header } from "@/components/header/header";
 
 const meta: Meta<typeof Header> = {
   component: Header,
+  args: {
+    text: "Header Text",
+  },
 };
 
 export default meta;
@@ -13,7 +16,7 @@ type Story = StoryObj<typeof Header>;
 export const Primary: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const headerText = canvas.getByText(/Language Day-by-Day/);
+    const headerText = canvas.getByText(/Header Text/);
     await expect(headerText).toBeInTheDocument();
   },
 };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,6 +4,6 @@ interface HeaderProps {
   text: string;
 }
 
-export const Header = (props: HeaderProps) => {
-  return <h1 className="text-3xl font-bold flex justify-center text-center">{props.text}</h1>;
+export const Header = ({text}: HeaderProps) => {
+  return <h1 className="text-3xl font-bold flex justify-center text-center">{text}</h1>;
 };
