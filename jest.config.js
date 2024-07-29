85244125977b57c11b14cf6fa652fc881a6bca95
sp1222 <s.polozoff@gmail.com>
2024-07-29 10:55:54 -0500

    revert jest config, add playwright to pipeline as-is

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-export const projects = [
-  {
-    displayName: 'jest',
-    testEnvironment: 'node',
-    testMatch: ['**/__test__/src/**/*.test.ts', '**/__test__/src/**/*.spec.ts'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-  },
-];
+module.exports = {
+    projects: [
+      {
+        displayName: 'jest',
+        testEnvironment: 'node',
+        testMatch: ['**/__test__/src/**/*.test.ts', '**/__test__/src/**/*.spec.ts'],
+        transform: {
+          '^.+\\.ts$': 'ts-jest',
+        },
+      },
+    ],
+  };
   
