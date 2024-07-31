4f36bfc4d683dd5abab4588bdccaa38938e26073
sp1222 <s.polozoff@gmail.com>
2024-07-30 19:20:50 -0500

    rename directory for screenshots

--- __test__/visual/components/header.test.ts
+++ __test__/visual/components/header.test.ts
@@ -6,9 +6,14 @@ test("Header visual regression test", async ({ page }) => {
   await page.goto("/iframe.html?viewMode=story&id=components-header--primary");
   await page.waitForTimeout(1000);
 
-  const baselinePath = resolve("__test__/visual/images/screenshots/header-primary-baseline.png");
-  const currentPath = resolve("__test__/visual/images/screenshots/header-primary-current.png");
-  const diffPath = resolve("__test__/visual/images/screenshots/header-primary-diff.png");
+  const baselinePath = resolve("__test__/visual/screenshots/header-primary-baseline.png");
+  const currentPath = resolve("__test__/visual/screenshots/header-primary-current.png");
+  const diffPath = resolve("__test__/visual/screenshots/header-primary-diff.png");
+
+  console.log('PATHS')
+  console.log(currentPath);
+  console.log(baselinePath);
+  console.log(diffPath);
 
   await page.screenshot({ path: currentPath });
   if (await isRunningRegressionTest(baselinePath, currentPath)) {
